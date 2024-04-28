3f1b7239db2212c23af5399afad52c20d2e81349
Michael Krumdick <mrkrumdick@gmail.com>
2024-04-28 09:39:03 -0400

    Using pytree structure to make nicer layers

--- main.py
+++ main.py
@@ -2,15 +2,108 @@ from dataclasses import dataclass
 import itertools
 from itertools import product
 import random
+from typing import List, Any
 
+from jax.tree_util import register_pytree_node_class
 import jax.numpy as jnp
 import jax.random
-import jax.nn
+
+
+@register_pytree_node_class
+@dataclass
+class Layer:
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        return cls(*children)
+
+
+@register_pytree_node_class
+@dataclass
+class EmbeddingMatrix(Layer):
+    M: jnp.array
+
+    def __call__(self, indeces):
+        return jnp.take(self.M, indeces, axis=0)
+
+    @classmethod
+    def initialize(cls, key, n, d):
+        key, subkey = jax.random.split(key, 2)
+        embedding_matrix = jax.random.normal(shape=(n, d), key=subkey)
+        return key, cls(embedding_matrix)
+
+    def tree_flatten(self):
+        return ((self.M,), None)
+
+
+@register_pytree_node_class
+@dataclass
+class Linear(Layer):
+    w: jnp.array
+    b: jnp.array
+    act_fn: Any
+
+    def __call__(self, x):
+        h = jnp.matmul(x, self.w) + self.b
+        return self.act_fn(h)
+
+    @classmethod
+    def initialize(cls, key, input_dim, output_dim, act):
+        key, *subkey = jax.random.split(key, 3)
+        weights = jax.random.normal(shape=(input_dim, output_dim), key=subkey[0])
+        bias = jax.random.normal(shape=(output_dim,), key=subkey[1])
+        return key, cls(weights, bias, act)
+
+    def tree_flatten(self):
+        return ((self.w, self.b), self.act_fn)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        return cls(*children, aux_data)
+
+
+@register_pytree_node_class
+@dataclass
+class Sequential(Layer):
+    layers: List[Layer]
+
+    def __call__(self, x):
+        inp = x
+        for layer in self.layers:
+            inp = layer(inp)
+        return inp
+
+    def tree_flatten(self):
+        return ((self.layers,), None)
+
+
+def softmax(x):
+    vals = jnp.exp(x)
+    totals = jnp.sum(vals, axis=-1, keepdims=True)
+    return vals / totals
+
+
+def nll_loss(probs, target):
+    log_probs = jnp.log(probs)
+    ll = jnp.take_along_axis(log_probs, target.reshape(-1, 1), axis=1).mean()
+    return -1.0 * ll
+
+
+def forward(embed, mlp, data):
+    left_embed = embed(data[:, 0])
+    right_embed = embed(data[:, 1])
+    target = data[:, 2]
+
+    embeded = jnp.concatenate([left_embed, right_embed], axis=1)
+    logits = mlp(embeded)
+    probs = softmax(logits)
+    loss_value = nll_loss(probs, target)
+    return loss_value
+
 
 train_per = 0.8
 p = 7
 
-modulo_addition = lambda x, y: x  # (x + y) % p
+modulo_addition = lambda x, y: (x + y) % p
 
 x = list(range(0, 7))
 y = list(range(0, 7))
@@ -32,69 +125,29 @@ key = jax.random.key(seed)
 key, subkey = jax.random.split(key)
 
 embed_dim = 8
-
-embedding_matrix = jax.random.normal(shape=(p, embed_dim), key=key)
-
-
-def embed(embedding_matrix, token_ids):
-    return jnp.take(embedding_matrix, token_ids, axis=0)
+hidden_dim = 32
 
 
-key, subkey = jax.random.split(key)
-l1_weights = jax.random.normal(shape=(2 * embed_dim, 32), key=key)
-key, subkey = jax.random.split(key)
-l1_bias = jax.random.normal(shape=(32,), key=key)
-
-key, subkey = jax.random.split(key)
-l2_weights = jax.random.normal(shape=(32, p), key=key)
-l2_bias = jax.random.normal(shape=(p,), key=key)
+key, embedding_layer = EmbeddingMatrix.initialize(key, p, embed_dim)
+key, layer_one = Linear.initialize(key, 2 * embed_dim, hidden_dim, jnp.tanh)
+key, layer_two = Linear.initialize(key, hidden_dim, p, lambda x: x)
+mlp = Sequential([layer_one, layer_two])
 
 
-def softmax(x):
-    vals = jnp.exp(x)
-    totals = jnp.sum(vals, axis=-1, keepdims=True)
-    return vals / totals
-
-
-def network(l1_weights, l1_bias, l2_weights, l2_bias, x):
-    h1 = jnp.tanh(jnp.matmul(x, l1_weights) + l1_bias)
-
-    out = jnp.matmul(h1, l2_weights) + l2_bias
-
-    probs = softmax(out)
-    return probs
-
-
-def nll_loss(probs, target):
-    log_probs = jnp.log(probs)
-    ll = jnp.take_along_axis(log_probs, target.reshape(-1, 1), axis=1).mean()
-    return -1.0 * ll
-
-
-def forward(embedding_matrix, l1_weights, l1_bias, l2_weights, l2_bias, data):
-    left_embed = embed(embedding_matrix, data[:, 0])
-    right_embed = embed(embedding_matrix, data[:, 1])
-    target = data[:, 2]
-
-    embeded = jnp.concatenate([left_embed, right_embed], axis=1)
-    probs = network(l1_weights, l1_bias, l2_weights, l2_bias, embeded)
-    loss_value = nll_loss(probs, target)
-    return loss_value
-
-
-grad_fn = jax.value_and_grad(forward, argnums=(0, 1, 2, 3, 4))
-params = (embedding_matrix, l1_weights, l1_bias, l2_weights, l2_bias)
+grad_fn = jax.value_and_grad(forward, argnums=(0, 1))
 
 lr = 0.1
 for epoch in range(10000):
     random.shuffle(train_data)
-    idx = 0
-    for jdx in range(0, len(train_data), 8):
+    for idx in range(0, len(train_data), 8):
         batch = train_data[idx : idx + 8]
 
         data = jnp.array(batch)
 
-        out = forward(*params, data)
-        loss_value, grads = grad_fn(*params, data)
+        loss_value, grads = grad_fn(embedding_layer, mlp, data)
         print(loss_value)
-        params = tuple(p - lr * g for p, g in zip(params, grads))
+
+        embedding_layer = jax.tree_map(
+            lambda p, g: p - lr * g, embedding_layer, grads[0]
+        )
+        mlp = jax.tree_map(lambda p, g: p - lr * g, mlp, grads[1])
