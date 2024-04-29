b795589a43034a3b10d24e36754c37f48cfbac56
Michael Krumdick <mrkrumdick@gmail.com>
2024-04-28 20:36:22 -0400

    Version with working eval

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ from jax.tree_util import register_pytree_node_class
 import jax.numpy as jnp
 import jax.random
 
+def layer(x):
+    return register_pytree_node_class(dataclass(x))
 
-@register_pytree_node_class
-@dataclass
+
+@layer
 class Layer:
     @classmethod
     def tree_unflatten(cls, aux_data, children):
         return cls(*children)
 
 
-@register_pytree_node_class
-@dataclass
+@layer
 class EmbeddingMatrix(Layer):
     M: jnp.array
-
+    
+    @jax.jit
     def __call__(self, indeces):
         return jnp.take(self.M, indeces, axis=0)
 
@@ -35,13 +37,13 @@ class EmbeddingMatrix(Layer):
         return ((self.M,), None)
 
 
-@register_pytree_node_class
-@dataclass
+@layer
 class Linear(Layer):
     w: jnp.array
     b: jnp.array
     act_fn: Any
 
+    @jax.jit
     def __call__(self, x):
         h = jnp.matmul(x, self.w) + self.b
         return self.act_fn(h)
@@ -61,8 +63,7 @@ class Linear(Layer):
         return cls(*children, aux_data)
 
 
-@register_pytree_node_class
-@dataclass
+@layer
 class Sequential(Layer):
     layers: List[Layer]
 
@@ -74,6 +75,11 @@ class Sequential(Layer):
 
     def tree_flatten(self):
         return ((self.layers,), None)
+
+
+def batch_iterator(array, batchsize):
+    for idx in range(0, len(array), batchsize):
+        yield array[idx : idx + batchsize]
 
 
 def softmax(x):
@@ -88,25 +94,40 @@ def nll_loss(probs, target):
     return -1.0 * ll
 
 
-def forward(embed, mlp, data):
-    left_embed = embed(data[:, 0])
-    right_embed = embed(data[:, 1])
-    target = data[:, 2]
-
-    embeded = jnp.concatenate([left_embed, right_embed], axis=1)
+def forward(embedding_layer, mlp, data):
+    embeded = mlp_embed(embedding_layer, data)
     logits = mlp(embeded)
     probs = softmax(logits)
+    return probs
+
+def compute_loss(embedding_layer, mlp, data):
+    probs = forward(embedding_layer, mlp, data)
+    target = data[:, 2]
     loss_value = nll_loss(probs, target)
     return loss_value
 
+def compute_acc(embedding_layer, mlp, data):
+    embeded = mlp_embed(embedding_layer, data)
+    target = data[:, 2]
+    logits = mlp(embeded)
+    probs = softmax(logits)
+    preds = jnp.argmax(probs, axis=1)
+    return preds == target
 
-train_per = 0.8
-p = 7
+def mlp_embed(embedding_layer, data):
+    left_embed = embedding_layer(data[:, 0])
+    right_embed = embedding_layer(data[:, 1])
+    embeded = jnp.concatenate([left_embed, right_embed], axis=1)
+    return embeded
 
-modulo_addition = lambda x, y: (x + y) % p
 
-x = list(range(0, 7))
-y = list(range(0, 7))
+train_per = 0.99
+p = 100
+
+modulo_addition = lambda x, y: x #(x + y) % p
+
+x = list(range(0, p))
+y = list(range(0, p))
 combos = list(itertools.product(x, y))
 
 data = list(map(lambda x: (*x, modulo_addition(*x)), combos))
@@ -116,38 +137,58 @@ random.shuffle(data)
 total = len(data)
 train_amount = int(train_per * total)
 
-train_data = data[:train_amount]
-test_data = data[train_amount:]
+train_data = jnp.array(data[:train_amount])
+test_data = jnp.array(data[train_amount:])
 
 seed = 123
 key = jax.random.key(seed)
 
 key, subkey = jax.random.split(key)
 
-embed_dim = 8
+embed_dim = 16
 hidden_dim = 32
 
 
 key, embedding_layer = EmbeddingMatrix.initialize(key, p, embed_dim)
-key, layer_one = Linear.initialize(key, 2 * embed_dim, hidden_dim, jnp.tanh)
-key, layer_two = Linear.initialize(key, hidden_dim, p, lambda x: x)
-mlp = Sequential([layer_one, layer_two])
 
+hidden_dims = [2 * embed_dim, 32, 32, 32, p]
 
-grad_fn = jax.value_and_grad(forward, argnums=(0, 1))
+layers = []
+for idx in range(len(hidden_dims) - 1):
+    act_fn = jnp.tanh
+    if idx == len(hidden_dims) - 2:
+        act_fn = lambda x: x
+    
+    key, layer = Linear.initialize(key, hidden_dims[idx], hidden_dims[idx + 1], act_fn)
+
+    layers.append(layer)
+
+mlp = Sequential(layers)
+
+grad_fn = jax.value_and_grad(compute_loss, argnums=(0, 1))
 
 lr = 0.1
 for epoch in range(10000):
-    random.shuffle(train_data)
-    for idx in range(0, len(train_data), 8):
-        batch = train_data[idx : idx + 8]
 
-        data = jnp.array(batch)
+    key, subkey = jax.random.split(key)
+    jax.random.permutation(subkey, train_data)
 
-        loss_value, grads = grad_fn(embedding_layer, mlp, data)
-        print(loss_value)
+    for batch in batch_iterator(train_data, 32):
+        loss_value, grads = grad_fn(embedding_layer, mlp, batch)
 
         embedding_layer = jax.tree_map(
             lambda p, g: p - lr * g, embedding_layer, grads[0]
         )
         mlp = jax.tree_map(lambda p, g: p - lr * g, mlp, grads[1])
+
+    print(loss_value)
+
+    correct = []
+    for batch in batch_iterator(test_data, 8):
+        data = jnp.array(batch)
+        is_correct = compute_acc(embedding_layer, mlp, data)
+        correct.append(is_correct)
+
+    correct = jnp.concatenate(correct)
+    print("TEST ACC, ", correct.mean())
+
